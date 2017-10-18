05f1004af5b347360d110e7f2f63ce7a2480f5fd
gaoshan12345 <123725011@qq.com>
2017-10-18 17:37:12 +0800

    first commit

---
+++ src/main/java/com/winksi/roinchina/controller/RoinChinaController.java
@@ -0,0 +1,41 @@
+package com.winksi.roinchina.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 
+* @ClassName: RoinChinaController
+* @Description: TODO积分Controller  
+* @author 王文龙  
+* @date 2017年7月18日  
+*
+ */
+@Controller
+public class RoinChinaController {
+
+	private static final Logger log = LoggerFactory.getLogger(RoinChinaController.class);
+
+	
+	
+	public RoinChinaController(){
+		log.info("RoinChinaController init");
+	}
+
+	/**
+	 * 查询归属地v1.0接口
+	 * 
+	 * @param input
+	 * @return
+	 */
+	@RequestMapping(value = "/getToken", method = RequestMethod.POST)
+	@ResponseBody
+	public JSONObject getToken(@RequestBody JSONObject input) {
+		JSONObject json = new JSONObject();
+		json.put("akey", "avalue");
+		return json;
+	}
+}
---
+++ src/main/java/com/zdws/spring/tx/AccountServiceImpl.java
@@ -0,0 +1,18 @@
+package com.zdws.spring.tx;
+ 
+/**
+ * 转账操作业务层实现类
+ *
+ */
+public class AccountServiceImpl implements AccountService {
+ 
+   
+ 
+    /**
+     * 转账
+     */
+    public void transfer(String out, String in, int money) {
+       
+    }
+ 
+}
