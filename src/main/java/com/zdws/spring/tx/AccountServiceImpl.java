60c7d66a2fafa6f51a58169582be18e7eb84da2d
gaoshan12345 <123725011@qq.com>
2017-10-19 11:02:52 +0800

    test

--- src/main/java/com/zdws/spring/tx/AccountServiceImpl.java
+++ src/main/java/com/zdws/spring/tx/AccountServiceImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zdws.spring.tx;
  
 /**
@@ -15,24 +14,4 @@ public class AccountServiceImpl implements AccountService {
     public void transfer(String out, String in, int money) {
        
     }
- 
-=======
-package com.zdws.spring.tx;
- 
-/**
- * 转账操作业务层实现类
- *
- */
-public class AccountServiceImpl implements AccountService {
- 
-   
- 
-    /**
-     * 转账
-     */
-    public void transfer(String out, String in, int money) {
-       
-    }
- 
->>>>>>> cd25a129ac6e9d3b2982c803a677aa7618e4c08a
 }
